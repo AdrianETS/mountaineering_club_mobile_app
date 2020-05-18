3e5325029b2f79301c1131e601843d1e8e773fc4
Adrian <adrian.belver@etservices.eu>
2020-05-18 17:55:48 +0200

    Edit member complete

--- components/EditMemberInfo.js
+++ components/EditMemberInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View, TextInput, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput, SafeAreaView, FlatList, TouchableOpacity, ScrollView } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import StorageProvider from "../services/StorageProvider";
@@ -7,77 +7,174 @@ import StorageProvider from "../services/StorageProvider";
 function getMemberInfo(id) {
     return new Promise((resolve, reject) => {
         StorageProvider.getData("token")
-        .then(token => fetch('http://192.168.1.134:3001/members/' + id + '?token=' + token))
+            .then(token => fetch('http://192.168.1.134:3001/members/' + id + '?token=' + token))
             .then(res => res.json())
             .then(json => resolve(json))
     })
 }
 
-function editMember(member) {
-    return new Promise((resolve, reject) => {
-        fetch('http://127.0.0.1:3001/members?token=' + this.getTokenFromLocalStorage(), {
-            method: 'PUT',
-            body: JSON.stringify({
-                _id: member._id,
-                name: member.name,
-                surname: member.surname,
-                birthDate: member.birthDate,
-                clubId: member.clubId,
-                licenseNumber: member.licenseNumber,
-                type: member.type,
-                password: member.password,
-                responsibilityAgreementSigned: (member.responsibilityAgreementSigned === "true" && true) || false
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        })
+function editMember(member, navigation) {
+    //return new Promise((resolve, reject) => {
+        StorageProvider.getData("token")
+            .then(token => fetch('http://192.168.1.134:3001/members?token=' + token, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    _id: member._id,
+                    name: member.name,
+                    surname: member.surname,
+                    email: member.email,
+                    birthDate: member.birthDate,
+                    clubId: member.clubId,
+                    licenseNumber: member.licenseNumber,
+                    type: member.type,
+                    password: member.password,
+                    responsibilityAgreementSigned: (member.responsibilityAgreementSigned === "true" && true) || false
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            }))
             .then(res => res.json())
-            .then(json => resolve(json));
-    })
+            .then(()=>navigation.navigate('Dashboard'))
+    
+}
+
+handleMemberAttribute = (text, attribute, onChangeUserInfo) =>{
+    //userInfo && (userInfo[attribute] = text);
+    /*buscamos el attribute por la key. userInfo.attribute sería incorrecto porque crearía uno nuevo
+    Esta la forma dinámica de JS para acceder al atributo de un objeto
+    */
+    onChangeUserInfo(prevState => ({ ...prevState, [attribute]: text }));
 }
 
 
-
-function EditMemberInfo({route, navigation}){
+function EditMemberInfo({ route, navigation }) {
     const { _id } = route.params;
     const [userInfo, onChangeUserInfo] = React.useState({});
-    const handleMemberName = (text) =>{
+
+    const handleMemberName = text => {
         userInfo && (userInfo.name = text);
-        onChangeUserInfo(userInfo);
+        onChangeUserInfo(prevState => ({ ...prevState, name: text }));
+    }
+
+    const handleMemberSurname = text => {
+        userInfo && (userInfo.surname = text);
+        onChangeUserInfo(prevState => ({ ...prevState, surname: text }));
+    }
+
+    const handleMemberEmail = text => {
+        userInfo && (userInfo.email = text);
+        onChangeUserInfo(prevState => ({ ...prevState, email: text }));
+    }
+
+    const handleMemberBirthDate = text => {
+        userInfo && (userInfo.birthDate = text);
+        onChangeUserInfo(prevState => ({ ...prevState, birthDate: text }));
+    }
+
+    const handleMemberClubId = text => {
+        userInfo && (userInfo.clubId = text);
+        onChangeUserInfo(prevState => ({ ...prevState, clubId: text }));
+    }
+
+    const handleMemberLicenseNumber = text => {
+        userInfo && (userInfo.licenseNumber = text);
+        onChangeUserInfo(prevState => ({ ...prevState, licenseNumber: text }));
+    }
+
+    const handleMemberType = text => {
+        userInfo && (userInfo.type = text);
+        onChangeUserInfo(prevState => ({ ...prevState, type: text }));
+    }
+
+    const handleMemberResponsibility = text => {
+        userInfo && (userInfo.responsibilityAgreementSigned = text);
+        onChangeUserInfo(prevState => ({ ...prevState, responsibilityAgreementSigned: text }));
+    }
+
+    const handleMemberPassword = text => {
+        userInfo && (userInfo.password = text);
+        onChangeUserInfo(prevState => ({ ...prevState, password: text }));
     }
 
     React.useEffect(() => {
         getMemberInfo(_id)
-        .then(userInfo => onChangeUserInfo(userInfo))
-      }, []);
+            .then(userInfo => onChangeUserInfo(userInfo))
+    }, []);
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Name{"\n"}</Text>
-        <TextInput
-        placeholder = {"name"}
-        style = {{height: 40, paddingLeft: "2%", width: "30%", borderColor: "gray", borderWidth: 1}}
-        onChangeText = {handleMemberName}
-        value = {userInfo.name}
-        />
-        <Text>{"\n"}</Text>
-        <Text>Surname{"\n"}</Text>
-        <TextInput
-        placeholder = {"Surname"}
-        style = {{height: 40, paddingLeft: "2%", width: "30%", borderColor: "gray", borderWidth: 1}}
-        value = {userInfo.surname}
-        />
-        <Text>{"\n"}</Text>
-        <Text>Email{"\n"}</Text>
-        <TextInput
-        placeholder = {"Email"}
-        style = {{height: 40, paddingLeft: "2%", width: "30%", borderColor: "gray", borderWidth: 1}}
-        value = {userInfo.email}
-        />
-        <Text>{"\n"}</Text>
+            <ScrollView
+                scrollEnabled={true}
+            >
+                <Text>Name{"\n"}</Text>
+                <TextInput
+                    placeholder={"name"}
+                    style={{ height: 40, paddingLeft: "2%", width: "70%", borderColor: "gray", borderWidth: 1 }}
+                    onChangeText={text => handleMemberAttribute(text, "name", onChangeUserInfo)}
+                    value={userInfo.name}
+                />
+                <Text>Surname{"\n"}</Text>
+                <TextInput
+                    placeholder={"Surname"}
+                    style={{ height: 40, paddingLeft: "2%", width: "70%", borderColor: "gray", borderWidth: 1 }}
+                    onChangeText={handleMemberSurname}
+                    value={userInfo.surname}
+                />
+                <Text>Email{"\n"}</Text>
+                <TextInput
+                    placeholder={"Email"}
+                    style={{ height: 40, paddingLeft: "2%", width: "70%", borderColor: "gray", borderWidth: 1 }}
+                    onChangeText={handleMemberEmail}
+                    value={userInfo.email}
+                />
+                <Text>Birth Date{"\n"}</Text>
+                <TextInput
+                    placeholder={"Email"}
+                    style={{ height: 40, paddingLeft: "2%", width: "70%", borderColor: "gray", borderWidth: 1 }}
+                    onChangeText={handleMemberBirthDate}
+                    value={userInfo.birthDate}
+                />
+                <Text>Club Id{"\n"}</Text>
+                <TextInput
+                    placeholder={"Club Id"}
+                    style={{ height: 40, paddingLeft: "2%", width: "70%", borderColor: "gray", borderWidth: 1 }}
+                    onChangeText={handleMemberClubId}
+                    value={userInfo.clubId}
+                />
+                <Text>License Number{"\n"}</Text>
+                <TextInput
+                    placeholder={"Club Id"}
+                    style={{ height: 40, paddingLeft: "2%", width: "70%", borderColor: "gray", borderWidth: 1 }}
+                    onChangeText={handleMemberLicenseNumber}
+                    value={userInfo.licenseNumber}
+                />
+                <Text>Type{"\n"}</Text>
+                <TextInput
+                    placeholder={"Club Id"}
+                    style={{ height: 40, paddingLeft: "2%", width: "70%", borderColor: "gray", borderWidth: 1 }}
+                    onChangeText={handleMemberType}
+                    value={userInfo.type}
+                />
+                <Text>Resposibility agreement signed?{"\n"}</Text>
+                <TextInput
+                    placeholder={"Responsibility agreement? True/False"}
+                    style={{ height: 40, paddingLeft: "2%", width: "70%", borderColor: "gray", borderWidth: 1 }}
+                    onChangeText={handleMemberResponsibility}
+                    value={String(userInfo.responsibilityAgreementSigned)}
+                />
+                <Text>Password{"\n"}</Text>
+                <TextInput
+                    placeholder={"Pass"}
+                    style={{ height: 40, paddingLeft: "2%", width: "70%", borderColor: "gray", borderWidth: 1 }}
+                    onChangeText={handleMemberPassword}
+                    value={""}
+                />
+                <Text>{"\n"}</Text>
+                <Button title="Edit info" Style={{ paddingBottom: 20 }} onPress={() => editMember(userInfo, navigation)} />
+            </ScrollView>
         </View>
-      );
+    );
 }
 
 export default EditMemberInfo;
